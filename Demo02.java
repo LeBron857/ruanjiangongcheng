bb2141c0ce526888e27446979062dfffc06914b5
LeBron857 <70878354+LeBron857@users.noreply.github.com>
2020-09-15 18:29:58 +0800

    Update Demo02.java

--- Demo02.java
+++ Demo02.java
@@ -1,20 +1,15 @@
-package demo;
+import java.util.Arrays;
 
-public class Demo02 {
-	public static void main(String[] args) {
-		int [] arr={34,25,88,65,130,558,1};
-		int zj=0;
-		for(int i=1;i<arr.length;i++){
-			for(int j=0;j<arr.length-i;j++)	{
-				if(arr[j]>arr[j+1]){
-					zj=arr[j+1];
-					arr[j+1]=arr[j];
-					arr[j]=zj;
-				}
-			}
-		}
-		for(int a=0;a<arr.length;a++){
-			System.out.print(arr[a]+"	");
-		}
-	}
+public class Demo001 {
+ public static void mpPx(int arr[]){
+  for (int i = 0; i <= arr.length - 1; i++){
+   for (int j = 0; j < arr.length - 1 - i;j++){
+    if (arr[j] > arr[j+1]){
+     int temp = arr[j];
+     arr[j] = arr[j+1];
+     arr[j+1] = temp;
+    }
+   }
+  }
+ }
 }
